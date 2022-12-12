0696ec37118f0ac643362de437bb95b095d0d686
kykyro <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>
2022-12-12 14:13:37 +0300

    кавычки при редактирование названия кластеров на карте

--- public/js/scripts/map_editor.js
+++ public/js/scripts/map_editor.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     let input = $('#map_edit_form_organization');
-    console.log(input.val());
     let mapEditor = $( '#map-editor');
     let obj = JSON.parse(input.val());
     let string = '';
@@ -23,7 +22,7 @@ $(document).ready(function(){
        years.forEach((elem)=>{
            let _data = elem.split(':');
            console.log(_data);
-           data[_data[0]] = {"organization": _data[1].trim()};
+           data[_data[0]] = {"organization": _data[1].trim().replaceAll('"', "'")};
        });
        input.val(JSON.stringify(data));
     })
