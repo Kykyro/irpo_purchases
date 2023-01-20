681a0f6b0ee3cb351ec905a1658eaae235e3602b
Kykyro <meiiiok51@gmail.com>
2023-01-20 11:09:49 +0300

    Страница мои кластеры

---
+++ src/Controller/Inspector/InspectorMyClustersController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Inspector;
+
+use App\Entity\RfSubject;
+use App\Entity\User;
+use App\Entity\UserInfo;
+use App\Form\InspectorPurchasesFindFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use App\Entity\ProcurementProcedures;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Services\XlsxService;
+
+/**
+ * Class InspectorController
+ * @package App\Controller
+ * @Route("/inspector")
+ */
+class InspectorMyClustersController extends AbstractController
+{
+    /**
+     * @Route("/my-clusters", name="app_inspector_my_clusters")
+     */
+    public function index(Request $request): Response
+    {
+
+
+
+
+        return $this->render('inspector/templates/myClusters.html.twig', [
+            'controller_name' => 'InspectorController'
+
+        ]);
+    }
+
+
+
+}
