2c77cad6896799b735dd7b7132bdacb7b365bd22
madhuron <maximluk56@gmail.com>
2023-03-06 13:00:35 +0300

    Изменение дизайна главной 2.1V

--- public/landing/js/main.js
+++ public/landing/js/main.js
@@ -631,8 +631,8 @@
                 responsive: [{
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
                         infinite: !0,
                         dots: !0
                     }
@@ -681,6 +681,46 @@
                     settings: {
                         slidesToShow: 2,
                         slidesToScroll: 2
+                    }
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }]
+            })
+        }))), void(e(".ed-carousel3").length > 0 && e(".ed-carousel3").each((function() {
+            var a = e(this),
+                t = a.data("arrows") || !0,
+                i = a.data("dots") || !0,
+                o = a.data("loop") || !1;
+            a.slick({
+                arrows: t,
+                dots: i,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 10000,
+                speed: 1200,
+                waitForAnimate: false,
+                slide: ".carousel-item3",
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                pauseOnHover: false,
+                cssEase: 'linear',
+                responsive: [{
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: !0,
+                        dots: !0
+                    }
+                }, {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
                     }
                 }, {
                     breakpoint: 480,
