60734a61f8cad824fb5af324fac0d33c424e029e
Kykyro <meiiiok51@gmail.com>
2023-03-16 15:02:02 +0300

    2022 год

--- src/Form/RegistrationUserInfoFormType.php
+++ src/Form/RegistrationUserInfoFormType.php
@@ -69,6 +69,7 @@ class RegistrationUserInfoFormType extends AbstractType
                 'choices'  => [
 
                     '2021' => 2021,
+                    '2022' => 2022,
                     '2023' => 2023,
                     '2024' => 2024,
 
