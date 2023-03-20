163df0e847ad95fb95fc7686c8142cc986e76984
Kykyro <meiiiok51@gmail.com>
2023-03-20 14:55:08 +0300

    Новые фильтры - зоны/угпс

--- src/Form/makeCertificateForm.php
+++ src/Form/makeCertificateForm.php
@@ -57,6 +57,8 @@ class makeCertificateForm extends AbstractType
                         'data-industry' => $user->getUserInfo()->getDeclaredIndustry(),
                         'data-base' => $user->getUserInfo()->getOrganization(),
                         'data-district' => is_null($user->getUserInfo()->getRfSubject()) ? '' : $user->getUserInfo()->getRfSubject()->getDistrict(),
+                        'data-zone' => json_encode($user->getUserInfo()->getZone()),
+                        'data-ugps' => json_encode($user->getUserInfo()->getUGPS()),
                     ]
                     : [];
                 }),
