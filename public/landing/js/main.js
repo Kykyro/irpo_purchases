0107c28c91f52a50f1fb49b36ad585d50e307b53
madhuron <maximluk56@gmail.com>
2023-03-20 10:06:05 +0300

    Изменение дизайна главной 2.7V

--- public/landing/js/main.js
+++ public/landing/js/main.js
@@ -791,6 +791,46 @@
                 responsive: [{
                     breakpoint: 3400,
                     settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: !0,
+                        dots: !0
+                    }
+                }, {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }]
+            })
+        }))), void(e(".ed-carousel6").length > 0 && e(".ed-carousel6").each((function() {
+            var a = e(this),
+                t = a.data("arrows") || !0,
+                i = a.data("dots") || !0,
+                o = a.data("loop") || !0;
+            a.slick({
+                arrows: t,
+                dots: i,
+                infinite: true,
+                autoplay: false,
+                autoplaySpeed: 10000,
+                speed: 1200,
+                waitForAnimate: false,
+                slide: ".carousel-item6",
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                pauseOnHover: false,
+                cssEase: 'linear',
+                responsive: [{
+                    breakpoint: 3400,
+                    settings: {
                         slidesToShow: 2,
                         slidesToScroll: 1,
                         infinite: !0,
