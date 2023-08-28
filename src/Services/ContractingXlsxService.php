1cf91536db9b2c8b0b11358d8ac6e49bad7e9d5a
kykyro <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>
2023-08-28 12:45:51 +0300

    исправление общего бюджета

--- src/Services/ContractingXlsxService.php
+++ src/Services/ContractingXlsxService.php
@@ -228,6 +228,7 @@ class ContractingXlsxService extends AbstractController
         $sheet->getStyle('A:Z')->getAlignment()->setVertical('center');
 
         $totalProcentageCell = (isset($row)) ? ($row+2) : 3;
+
         if($role == 'lot_1')
             $totalProcentageRow = $this->getTotalProcentageRow($year, '%ROLE_SMALL_CLUSTER_LOT_1%', $totalProcentageCell);
         else if($role == 'lot_2')
