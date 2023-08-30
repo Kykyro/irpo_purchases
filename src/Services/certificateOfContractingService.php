507d83d0aa4e84f8fbe7e4d1b2400111aa842614
kykyro <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>
2023-08-30 14:07:47 +0300

    необязательные поля в раотадателях

--- src/Services/certificateOfContractingService.php
+++ src/Services/certificateOfContractingService.php
@@ -183,11 +183,19 @@ class certificateOfContractingService extends AbstractController
         $fileName = $userInfo->getOrganization().'_'.$today->format('d.m.Y').'.pdf';
         $filepath = $templateProcessor->save();
 
-        Settings::setPdfRendererName(Settings::PDF_RENDERER_TCPDF);
-        Settings::setPdfRendererPath('vendor/tecnickcom');
-        $phpWord = IOFactory::load($filepath, 'Word2007');
-        $phpWord->save('document.pdf', 'PDF');
+        Settings::setPdfRendererName(Settings::PDF_RENDERER_MPDF);
+        Settings::setPdfRendererPath('.');
+        Settings::setOutputEscapingEnabled(true);
+        Settings::setMeasurementUnit(Settings::UNIT_MM);
+//        Settings::setCompatibility(true);
 
-        return $this->file($phpWord, $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
+        Settings::setDefaultFontName('dejavu sans');
+        Settings::setDefaultFontSize(12);
+
+        $phpWord = IOFactory::load($filepath);
+//        $phpWord->setDefaultFontName('dejavu sans');
+
+//        return $this->file($filepath,  $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
+        return $this->file($phpWord->save($fileName, 'PDF', true), $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
     }
 }
