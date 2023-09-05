bb8cf91f8039beae250ba31f3085b207e702bba0
kykyro <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>
2023-09-05 13:14:38 +0300

    Добавлены новые года в актуальные выгрузки

--- src/Controller/SmallCurator/SmallCuratorContractingController.php
+++ src/Controller/SmallCurator/SmallCuratorContractingController.php
@@ -52,6 +52,8 @@ class SmallCuratorContractingController extends AbstractController
                     '2021 год' => 2021,
                     '2022 год' => 2022,
                     '2023 год' => 2023,
+                    '2024 год' => 2024,
+                    '2025 год' => 2025,
 
                 ],
             ])
