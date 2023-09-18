9351c0073072672d0b9d1e6d24b8aeddbfe2089e
kykyro <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>
2023-09-18 17:01:54 +0300

    Текстовые поля названий в ИЛе - textarea

--- src/Form/infrastructureSheetType.php
+++ src/Form/infrastructureSheetType.php
@@ -11,6 +11,7 @@ use App\Entity\ZoneInfrastructureSheet;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,7 +22,7 @@ class infrastructureSheetType  extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
+            ->add('name', TextareaType::class, [
                 'attr' => [
                     'class' => 'form-control',
 
