f13ac849fcc5da9fe5a7f65cd1b3c14e737911aa
kykyro <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>
2024-03-19 11:57:46 +0300

    Контрактация для БАС

---
+++ src/Controller/BasCurator/BasCuratorContractingController.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Controller\BasCurator;
+
+use App\Entity\ContractingTables;
+use App\Entity\ProcurementProcedures;
+use App\Entity\ReadinessMapSaves;
+use App\Entity\User;
+use App\Services\ContractingXlsxService;
+use App\Services\ReadinessMapXlsxService;
+use App\Services\XlsxPerformanceIndicatorService;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+/**
+ *
+ * @package App\Controller
+ * @Route("/bas-curator")
+ */
+class BasCuratorContractingController extends AbstractController
+{
+    /**
+     * @Route("/contracting-actual", name="app_bas_curator_contracting")
+     */
+    public function index(Request $request, ContractingXlsxService $contractingXlsxService,
+                          ReadinessMapXlsxService $readinessMapXlsxService, XlsxPerformanceIndicatorService $indicatorService): Response
+    {
+
+        $arr = [];
+        $form = $this->createFormBuilder($arr)
+            ->add('year', ChoiceType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'choices'  => [
+
+                    '2024 год' => 2024,
+                    '2025 год' => 2025,
+                    '2026 год' => 2026,
+
+                ],
+            ])
+            ->add('type', ChoiceType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'choices'  => [
+                    'Контрактация' => 1,
+//                    'Карта готовности(все)' => 2,
+//                    'Карта готовности(ремонт)' => 3,
+//                    'Карта готовности(оборудование)' => 4,
+//                    'Показатели результативности' => 5,
+
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ],
+                'label' => 'Скачать'
+            ])
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'required'   => false,
+                'label' => 'Дата проверки'
+            ])
+            ->add('start', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    "min"  => 0,
+                    'max' => 200,
+                    'step' => 10
+                ],
+                'label' => 'Старт',
+                'empty_data' => 0,
+                'required'   => false,
+
+            ])
+            ->add('step', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    "min"  => 0,
+                    'max' => 200,
+                    'step' => 10
+                ],
+                'label' => 'Шаг',
+                'empty_data' => 0,
+                'required'   => false,
+
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            if($data['type'] == 1)
+            {
+                if($data['date'])
+                {
+                    return $contractingXlsxService->downloadTableBAS($data['year'], $data['date'], 'bas');
+                }
+                $today = new \DateTime('now');
+                return $contractingXlsxService->downloadTableBAS($data['year'], $today, 'bas');
+            }
+
+//            if($data['type'] == 2)
+//            {
+//                return $readinessMapXlsxService->downloadTable($data['year']);
+//            }
+//            if($data['type'] == 3)
+//            {
+//                return $readinessMapXlsxService->downloadTableRepair($data['year']);
+//            }
+//            if($data['type'] == 4)
+//            {
+//                return $readinessMapXlsxService->downloadTableEquipment($data['year'], 'cluster', false, $data['start'], $data['step']);
+//            }
+//            if($data['type'] == 5){
+//                return $indicatorService->generateTable($data['year'], "ROLE_REGION");
+//            }
+
+
+        }
+
+        return $this->render('inspector_contracting/index.html.twig', [
+            'controller_name' => 'InspectorContractingController',
+            'form' => $form->createView(),
+            'role' => 'bas'
+        ]);
+    }
+//    /**
+//     * @Route("/contracting-history", name="app_inspector_contracting_history")
+//     */
+    public function history(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
+    {
+
+        $dict = [
+            'cluster'=> 'Контрактация (Производственные кластеры)' ,
+            'lot_1' => 'Контрактация (Малые кластеры Лот 1)',
+            'lot_2' => 'Контрактация (Малые кластеры Лот 2)' ,
+        ];
+        $query = $entityManager->getRepository(ContractingTables::class)
+            ->createQueryBuilder('a')
+            ;
+
+        $query = $query->getQuery();
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            20 /*limit per page*/
+        );
+
+
+        return $this->render('inspector_contracting/history.html.twig', [
+            'controller_name' => 'InspectorContractingController',
+            'contracts' => $pagination,
+            'dict' => $dict,
+            'download_path' => 'contracting_tables_directory',
+        ]);
+    }
+    /**
+     * @Route("/rm-history", name="app_inspector_rm_history")
+     */
+    public function history_rm(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
+    {
+
+        $dict = [
+            'cluster'=> 'Карты готовности (Производственные кластеры)' ,
+            'lot_1' => 'Карты готовности (Малые кластеры Лот 1)',
+            'lot_2' => 'Карты готовности (Малые кластеры Лот 2)' ,
+        ];
+        $query = $entityManager->getRepository(ReadinessMapSaves::class)
+            ->createQueryBuilder('a')
+            ;
+
+        $query = $query->getQuery();
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            20 /*limit per page*/
+        );
+
+
+        return $this->render('inspector_contracting/history.html.twig', [
+            'controller_name' => 'InspectorContractingController',
+            'contracts' => $pagination,
+            'dict' => $dict,
+            'download_path' => 'readiness_map_saves_directory',
+        ]);
+    }
+}
--- src/Services/ContractingXlsxService.php
+++ src/Services/ContractingXlsxService.php
@@ -113,6 +113,12 @@ class ContractingXlsxService extends AbstractController
             $grant = 60500000;
             $users = $this->getUsersByYear($year, '%ROLE_SMALL_CLUSTERS_LOT_2%');
         }
+        else if($role == 'bas')
+        {
+            $fileName = 'Контрактация БАС '.$year." год ".$today->format('d-m-Y');
+            $grant = 60500000;
+            $users = $this->getUsersByYear($year, '%ROLE_BAS%');
+        }
         else
         {
             $fileName = 'Контрактация ОПЦ '.$year." год ".$today->format('d-m-Y');
@@ -242,6 +248,186 @@ class ContractingXlsxService extends AbstractController
 
 
         $sheet->fromArray($totalProcentageRow, null, 'E'.$totalProcentageCell);
+
+
+        // Запись файла
+        $writer = new Xlsx($spreadsheet);
+
+
+
+        if($save)
+        {
+//            dd();
+            $fileName = $fileName.'_'.uniqid().'.xlsx';
+            if (!file_exists($this->getParameter('contracting_tables_directory'))) {
+                mkdir($this->getParameter('contracting_tables_directory'), 0777, true);
+            }
+
+            $writer->save($this->getParameter('contracting_tables_directory').'/'.$fileName);
+
+            return $fileName;
+//            contracting_tables_directory
+//            return $this->file($temp_file, $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
+        }
+        else{
+//            dd();
+            $fileName = $fileName.'.xlsx';
+
+
+            $temp_file = tempnam(sys_get_temp_dir(), $fileName);
+
+            $writer->save($temp_file);
+
+            return $this->file($temp_file, $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
+        }
+
+    }
+    public function downloadTableBAS(int $year, \DateTime $today = null, string $role = 'cluster', $save = null)
+    {
+
+        $sheet_template = $this->getParameter('contrating_template_file');
+        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($sheet_template);
+        $sheet = $spreadsheet->getActiveSheet();
+        if(is_null($today))
+            $today = new \DateTime('now');
+
+        $index = 1;
+
+
+        $fileName = 'Контрактация БАС '.$year." год ".$today->format('d-m-Y');
+        $grant = 60500000;
+        $users = $this->getUsersByYear($year, '%ROLE_BAS%');
+
+        $curency_cell = ['E', 'G', 'I', 'K', 'M', 'N', 'O', 'P', 'L'];
+        foreach ($users as $user)
+        {
+            $procedures = $this->getProcedures($user);
+            $_data = [
+                'P' => 0,
+                'Q' => 0,
+                'R' => 0,
+                'M' => 0,
+                'N' => 0,
+                'O' => 0,
+                'I' => 0,
+                'G' => 0
+            ];
+            foreach ($procedures as $procedure)
+            {
+
+                if($procedure->getPurchasesStatus($today) == 'contract')
+                {
+                    $_data['G'] += $procedure->getfinFederalFunds();
+                    $_data['N'] += $procedure->getfinFundsOfSubject();
+                    $_data['M'] += $procedure->getfinEmployersFunds();
+                    $_data['O'] += $procedure->getfinFundsOfEducationalOrg();
+
+                }
+                elseif ($procedure->getPurchasesStatus($today) == 'announced')
+                {
+                    $_data['I'] += $procedure->getInitialFederalFunds();
+                    $_data['P'] += $procedure->getInitialEmployersFunds();
+                    $_data['Q'] += $procedure->getInitialFundsOfSubject();
+                    $_data['R'] += $procedure->getInitialEducationalOrgFunds();
+                }
+            }
+            $user_info = $user->getUserInfo();
+            $row = $sheet->getHighestRow()+1;
+            $sheet->setCellValue('A'.$row, $index);
+            $user_info_arr = [
+                $user_info->getRfSubject()->getName(),
+                $user_info->getDeclaredIndustry(),
+                $user_info->getEducationalOrganization()
+            ];
+            $sheet->fromArray($user_info_arr, null, 'B'.$row, true);
+            $other_arr = [
+
+                $_data['G'],
+                '=E'.$row.'/'.$grant,
+                $_data['I'],
+                '=G'.$row.'/'.$grant,
+                '=E'.$row.'+G'.$row,
+                '=F'.$row.'+H'.$row,
+                $_data['M'],
+                $_data['N'],
+                $_data['O'],
+                $_data['P'],
+                $_data['Q'],
+                $_data['R'],
+                '',
+                '',
+                '',
+            ];
+            $sheet->fromArray($other_arr, null, 'E'.$row, true);
+
+            $sheet->getStyle("F$row")->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_PERCENTAGE_00);
+            $sheet->getStyle("H$row")->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_PERCENTAGE_00);
+            $sheet->getStyle("J$row")->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_PERCENTAGE_00);
+//            $curency_cell = ['E', 'G', 'I', 'K', 'M', 'N', 'O', 'P'];
+            foreach ($curency_cell as $cell)
+            {
+                $sheet->getStyle("$cell$row")->getNumberFormat()->setFormatCode('#,##0.00_-"₽"');
+            }
+
+            $sheet->getRowDimension($index+1)->setRowHeight(65);
+            $index++;
+        }
+
+        $styleArray = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+            ],
+            'font' => [
+                'size'  => 14,
+                'name'  => 'Times New Roman'
+            ]
+        ];
+
+        $end_cell = $index;
+        $rangeTotal = 'A2:S'.($end_cell+2);
+        $sheet->getStyle($rangeTotal)->applyFromArray($styleArray);
+        $sheet->getStyle($rangeTotal)->getAlignment()->setWrapText(true);
+
+        $sumRow = $end_cell + 1;
+//        $sheet->setCellValue('G'.$sumRow, "=SUM(G2:G$end_cell)");
+//        $sheet->setCellValue('I'.$sumRow, "=SUM(I2:I$end_cell)");
+//        $sheet->setCellValue('K'.$sumRow, "=SUM(K2:K$end_cell)");
+//        $sheet->setCellValue('M'.$sumRow, "=SUM(M2:M$end_cell)");
+//        $sheet->setCellValue('N'.$sumRow, "=SUM(N2:M$end_cell)");
+//        $sheet->setCellValue('O'.$sumRow, "=SUM(O2:O$end_cell)");
+//        $sheet->setCellValue('P'.$sumRow, "=SUM(P2:P$end_cell)");
+//        $sheet->setCellValue('Q'.$sumRow, "=SUM(Q2:Q$end_cell)");
+//        $sheet->setCellValue('R'.$sumRow, "=SUM(R2:R$end_cell)");
+
+//        $curency_cell = ['G', 'I', 'K', 'M', 'N', 'O', 'P', 'Q', 'R'];
+        foreach ($curency_cell as $cell)
+        {
+            $sheet->setCellValue($cell.$sumRow, "=SUM({$cell}2:{$cell}$end_cell)");
+            $sheet->getStyle("$cell$sumRow")->getNumberFormat()->setFormatCode('#,##0.00_-"₽"');
+            $sheet->getStyle("$cell".($sumRow+1))->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_PERCENTAGE_00);
+        }
+
+        $sheet->getStyle('A:Z')->getAlignment()->setHorizontal('center');
+        $sheet->getStyle('A:Z')->getAlignment()->setVertical('center');
+
+        $totalProcentageCell = (isset($row)) ? ($row+2) : 3;
+        if($role == 'lot_1')
+            $totalProcentageRow = $this->getTotalProcentageRow($year, '%ROLE_SMALL_CLUSTER_LOT_1%', $totalProcentageCell);
+        else if($role == 'lot_2')
+            $totalProcentageRow = $this->getTotalProcentageRow($year, '%ROLE_SMALL_CLUSTER_LOT_2%', $totalProcentageCell);
+        else
+            $totalProcentageRow = $this->getTotalProcentageRow($year, '%REGION%', $totalProcentageCell);
+
+
+        $sheet->fromArray($totalProcentageRow, null, 'E'.$totalProcentageCell);
+
+        $deleteCol = ['P', 'N', 'M', 'K', 'D', 'C'];
+        foreach ($deleteCol as $i)
+        {
+            $sheet->removeColumn($i);
+        }
 
 
         // Запись файла
