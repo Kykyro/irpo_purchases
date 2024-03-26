9fddcc39bc1b70d7048e665321ba74658a0c090a
kykyro <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>
2024-03-26 10:34:08 +0300

    Какая-то ошибка

--- src/Services/ContractingXlsxService.php
+++ src/Services/ContractingXlsxService.php
@@ -237,22 +237,22 @@ class ContractingXlsxService extends AbstractController
         
         $sheet->getStyle('A:Z')->getAlignment()->setHorizontal('center');
         $sheet->getStyle('A:Z')->getAlignment()->setVertical('center');
-        try {
-            $totalProcentageCell = (isset($row)) ? ($row+2) : 3;
-            if($role == 'lot_1')
-                $totalProcentageRow = $this->getTotalProcentageRow($year, '%ROLE_SMALL_CLUSTER_LOT_1%', $totalProcentageCell);
-            else if($role == 'lot_2')
-                $totalProcentageRow = $this->getTotalProcentageRow($year, '%ROLE_SMALL_CLUSTER_LOT_2%', $totalProcentageCell);
-            else
-                $totalProcentageRow = $this->getTotalProcentageRow($year, '%REGION%', $totalProcentageCell);
-
-
-            $sheet->fromArray($totalProcentageRow, null, 'E'.$totalProcentageCell);
-        }
-        catch (\Exception $e)
-        {
-            echo 'PHP перехватил исключение: ',  $e->getMessage(), "\n";
-        }
+//        try {
+//            $totalProcentageCell = (isset($row)) ? ($row+2) : 3;
+//            if($role == 'lot_1')
+//                $totalProcentageRow = $this->getTotalProcentageRow($year, '%ROLE_SMALL_CLUSTER_LOT_1%', $totalProcentageCell);
+//            else if($role == 'lot_2')
+//                $totalProcentageRow = $this->getTotalProcentageRow($year, '%ROLE_SMALL_CLUSTER_LOT_2%', $totalProcentageCell);
+//            else
+//                $totalProcentageRow = $this->getTotalProcentageRow($year, '%REGION%', $totalProcentageCell);
+//
+//
+//            $sheet->fromArray($totalProcentageRow, null, 'E'.$totalProcentageCell);
+//        }
+//        catch (\Exception $e)
+//        {
+//            echo 'PHP перехватил исключение: ',  $e->getMessage(), "\n";
+//        }
 
 
         // Запись файла
