93f5b172355347aa00ac8985d282ad8d0e2e9e72
kykyro <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>
2024-04-09 16:06:54 +0300

    исправление команд

--- src/Command/weeklyContractCertificateBasCommand.php
+++ src/Command/weeklyContractCertificateBasCommand.php
@@ -20,13 +20,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Shapecode\Bundle\CronBundle\Annotation\CronJob;
 
 
-/**
- * @CronJob("5 3 * * 2")
- */
-class weeklyContractCertificateCommand extends Command
+
+class weeklyContractCertificateBasCommand extends Command
 {
 
-    protected static $defaultName = 'app:certificate:new';
+    protected static $defaultName = 'app:certificate:bas';
 
     private $serializer;
     private $entity_manager;
@@ -57,10 +55,9 @@ class weeklyContractCertificateCommand extends Command
         $clusters = $this->entity_manager->getRepository(User::class)
             ->createQueryBuilder('a')
             ->leftJoin('a.user_info', 'uf')
-            ->andWhere('a.roles LIKE :role_1 OR a.roles LIKE :role_2')
+            ->andWhere('a.roles LIKE :role_3')
             ->andWhere('uf.accessToPurchases = :access')
-            ->setParameter('role_1', "%REGION%")
-            ->setParameter('role_2', "%SMALL_CLUSTERS%")
+            ->setParameter('role_3', "%ROLE_BAS%")
             ->setParameter('access', true)
             ->getQuery()
             ->getResult();
